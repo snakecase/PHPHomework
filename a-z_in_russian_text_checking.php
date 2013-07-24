a79738559eea1b5775d0e55a3999615b15ba9235
Aristarkh Teterkin <shilo1991@gmail.com>
2013-07-24 14:27:17 +0400

    Refactored a bit

--- a-z_in_russian_text_checking.php
+++ a-z_in_russian_text_checking.php
@@ -1,19 +1,23 @@
 <?php
- 
-error_reporting(-1);
-mb_internal_encoding('utf-8');
 
-$text = "�oc�a��a ��co �o������, �ec�����oe ��� ���� ���������������� ����������
-���������� ��������������� ���������� ����o������� ������������ �����������
-�������� � 4 ������ ���";
+error_reporting(-1);
+
+$text = <<<EOF
+Пocтaвкa мяco гoвядины, бecкостнoe для нужд государственного бюджетного
+учреждения здравоохранения Республики Башкoртостан Инфекционная клиническая
+больница № 4 города Уфа
+3opя.
+12;Ёлкa;12
+EOF
+;
+
+$regexp = '/[а-яё]*[a-z3]+[а-яёa-z3]*/ui';
+$regexpLatinLetter = '/[a-z3]/i';
  
-$regexp = '/[�-�]*[a-z]+[�-�a-z]*/ui';
-$regexpError = '/[a-z]+/';
-preg_match_all($regexp, $text, $text);
+preg_match_all($regexp, $text, $matches, PREG_SET_ORDER);
  
-foreach ($text[0] as $word) {
-    if (preg_match($regexpError, $word)) {
-        $word = preg_replace($regexpError, '[$0]', $word);
-        echo "{$word}\n";
-    }
+foreach ($matches as $array) {
+    echo "Опечатка в слове {$array[0]}: ";
+    echo preg_replace($regexpLatinLetter, '[$0]', $array[0]);
+    echo "\n";
 }
